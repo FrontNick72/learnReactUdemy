18ebbe8a590438f7c64d6698d39802629232cf5d
Demyanenko Nikita <nd@crtweb.com>
2019-02-12 14:49:18 +0500

    adding price and adding toolbar

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -21,6 +21,7 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
             <Button clicked={props.purchaseCanceled} btnType='Danger'>Cancel</Button>
             <Button clicked={props.purchaseContinue} btnType='Success'>Continue</Button>
