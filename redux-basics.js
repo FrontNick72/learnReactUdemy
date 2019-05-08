7ad94dfbed6b24ddb1c8beb3f60f5a4a88c28c99
Demyanenko Nikita <nd@crtweb.com>
2019-05-08 15:48:45 +0500

    add dispatch and subcribe

--- redux-basics.js
+++ redux-basics.js
@@ -7,6 +7,20 @@ const inititalState = {
 
 //Reducer
 const rootReducer = (state = inititalState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        };
+    }
+
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
+
     return state;
 };
 
@@ -14,6 +28,12 @@ const rootReducer = (state = inititalState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-//Dispatching action
-
 //Subscription
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+});
+
+//Dispatching action
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+console.log(store.getState());
